e0ffb506b2532e5fb062c97ee6c3972cee3ca26b
Yorvi <jorvinrosario7@gmail.com>
2018-03-22 13:23:38 -0400

    made my Javascript Event a Callback, thanks to Chris I am all done

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
-function changeBG(element,color){
-	var element = document.getElementById(element);
-	element.style.transition = "background 1.0s linear 0s";
-	element.style.background = color;
-}
+
+
+// function changeBG(element,color){
+// 	var element = document.getElementById(element);
+// 	element.style.transition = "background 1.0s linear 0s";
+// 	element.style.background = color;
+// }
+
+document.getElementById("btn1").addEventListener('click', function() {
+	document.body.style.backgroundColor = "#757899"
+})
+
+// HUGE HELP FROM CHRIS!
+
+document.getElementById("btn2").addEventListener('click', function() {
+	document.body.style.backgroundColor = "#FEFFEF"
+})
+
+

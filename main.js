52071caca4aea367dfca0b7841eebb9483532d65
Yorvi <jorvinrosario7@gmail.com>
2018-03-27 10:34:02 -0400

    added a transition to Night Mode button, fixed html semantic elements

--- main.js
+++ main.js
@@ -8,12 +8,14 @@
 
 document.getElementById("btn1").addEventListener('click', function() {
 	document.body.style.backgroundColor = "#757899"
+	document.body.style.transition = "background 1.5s linear 0s"
 })
 
 // HUGE HELP FROM CHRIS!
 
 document.getElementById("btn2").addEventListener('click', function() {
 	document.body.style.backgroundColor = "#FEFFEF"
+	document.body.style.transition = "background 1.5s linear 0s"
 })
 
 

617023d98251f16e55d8498a3f477bd2ecb0c306
Yorvi <jorvinrosario7@gmail.com>
2018-04-02 19:39:07 -0400

    Final re-touches

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 
-
+// ORGINAL FUNCTION
 // function changeBG(element,color){
 // 	var element = document.getElementById(element);
 // 	element.style.transition = "background 1.0s linear 0s";
 // 	element.style.background = color;
 // }
 
+// CURRENT FUNCTION
 document.getElementById("btn1").addEventListener('click', function() {
 	document.body.style.backgroundColor = "#757899"
 	document.body.style.transition = "background 1.5s linear 0s"
